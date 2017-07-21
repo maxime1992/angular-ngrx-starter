0a3ece066f538af7f408c26150f80b65093e9301
Maxime ROBERT <maxime.robert1992@gmail.com>
2017-07-21 22:51:58 +0200

    chore: update material and add some basics rxjs operators by default

--- src/app/core/rxjs-operators.ts
+++ src/app/core/rxjs-operators.ts
@@ -18,3 +18,8 @@ import 'rxjs/add/operator/withLatestFrom';
 import 'rxjs/add/operator/combineLatest';
 import 'rxjs/add/operator/takeUntil';
 import 'rxjs/add/operator/ignoreElements';
+import 'rxjs/add/operator/mergeMap';
+import 'rxjs/add/operator/finally';
+import 'rxjs/add/operator/do';
+import 'rxjs/add/operator/first';
+import 'rxjs/add/operator/startWith';
