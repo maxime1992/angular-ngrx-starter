caa0752d1db21ab6b13a45ca086aef2f23e0038d
Maxime ROBERT <maxime.robert1992@gmail.com>
2017-09-24 19:06:57 +0200

    improvement: do not lazy load the features module
    There's no need to do that and having to download another js file is not helping

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,21 +7,26 @@ import {
 } from '@angular/router';
 
 import { RuntimeEnvironmentService } from 'app/core/runtime-environment.service';
+import { FeaturesModule } from 'app/features/features.module';
 import { environment } from 'environments/environment';
 
-// if you don't want to lazy load the features module,
-// simply put the loadFeaturesModule as value of loadChildren
-// import { FeaturesModule } from './features/features.module';
+// if you want to lazy load the features module,
+// 1 - remove the `import { FeaturesModule } from ...`;
+// 2 - remove the loadFeaturesModule function
 
-// export function loadFeaturesModule() {
-//   return FeaturesModule;
-// }
+// we cannot use shorthand like that:
+// loadChildren: () => FeaturesModule
+// to be AOT compliant, we need to create
+// a named function and export the module
+export function loadFeaturesModule() {
+  return FeaturesModule;
+}
 
 const routes: Routes = [
   {
     path: '',
     canActivate: [RuntimeEnvironmentService],
-    loadChildren: 'app/features/features.module#FeaturesModule',
+    loadChildren: loadFeaturesModule,
   },
 ];
 
